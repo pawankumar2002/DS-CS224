df5ef584fb6791a13a4e0d6eb8170389563a77ca
Pawan Kumar <pawankumar.gzp2002@gmail.com>
2022-02-11 16:37:32 +0530

    Initial Commit

--- Project/chloropleth.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import json
-
-india_states = json.load(open('states_india.geojson', 'r'))
-
-print(india_states['features'][0])
--- dsproject/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def home(req):
-    context = {}
-    return render(req, 'home.html', context)
